545825855d78189845a111935eee75497138512c
JongwooParks <mwoaoint0124@gmail.com>
2022-10-11 12:37:49 +0900

    test: 문자열 구분자 테스트

---
+++ src/test/java/junitstudy/StringTest.java
@@ -0,0 +1,23 @@
+package junitstudy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("구분점 있을때")
+    public void split(){
+
+        // given
+        String input = "1,2";
+
+        // when
+        String[] result = input.split(",");
+
+        // then
+        assertThat(result).containsExactly("1", "2");
+    }
+}
